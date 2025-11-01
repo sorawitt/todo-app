696805b023bb7a8ca9d36bb4f1630078536cf7e9
Sorawit <sorawit.tr@gmail.com>
2025-11-01 22:14:29 +0700

    style(todos): polish list ui

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,6 +4,19 @@ interface TodoItemProps {
   todo: Todo;
 }
 function TodoItem({ todo }: TodoItemProps) {
-  return <div>{todo.title}</div>;
+  return (
+    <div>
+      <label className="flex items-center gap-2 border rounded-md p-2 border-gray-300 bg-white hover:bg-slate-50">
+        <input
+          type="checkbox"
+          className="scale-125 mr-2"
+          defaultChecked={todo.completed}
+        />
+        <span className={todo.completed ? "line-through text-gray-500" : ""}>
+          {todo.title}
+        </span>
+      </label>
+    </div>
+  );
 }
 export default TodoItem;
