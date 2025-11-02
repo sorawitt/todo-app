00c0d526a4f368afe63d296b4bbf668c5fd746a8
Sorawit <sorawit.tr@gmail.com>
2025-11-02 14:38:43 +0700

    test(hook): add vitest setup and useTodos coverage

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.ts',
+    globals: true,
+    css: false,
+    coverage: {
+      provider: 'v8',
+    },
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+    ],
+  },
+})
